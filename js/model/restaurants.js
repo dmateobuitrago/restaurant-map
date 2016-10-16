b55856cfba8f60995f7eac31006e761c11d753b1
Mateo Buitrago <dmateobuitrago@gmail.com>
2016-10-16 09:21:13 -0500

    Initial working version of the app featuring:     - Hard coded restaurants array with location     - localStorage saving     - Marker default positioning     - StreetView panorama displays in infoWindow     - List display with knockout bindings     - Map styling

--- js/model/restaurants.js
+++ js/model/restaurants.js
@@ -1,82 +1,65 @@
-
-
 function geoCodeLocations() {
     if (!localStorage.restaurants) {
         console.log('Searching for coordinates...');
-        var geocoder;
         // hard coded restaurants
         var restaurants = [
             {
-                'name': 'Nobu',
+                'name': 'Nobu Yaki',
                 'facebook': 'https://www.facebook.com/nobu.yaki',
                 'address': 'Alameda de los Descalzos #90-16, Bogotá',
                 'cusine': 'Sushi',
+                'location': {lat: 4.6741453, lng: -74.05227730000001}
             },{
                 'name': 'Lai fu',
                 'facebook': 'https://www.facebook.com/LAI-FU-comida-china-386610398215900/',
                 'address': 'Cra. 50 #14966, Bogotá',
                 'cusine': 'Chinese',
+                'location': {'lat': 4.7322355, 'lng': -74.05299919999999}
             },{
                 'name': 'Mr Bross',
                 'facebook': 'https://www.facebook.com/MrBross-250044611853531/',
                 'address': 'Cl. 116 #19a32, Bogotá',
                 'cusine': 'Fast Food',
+                'location': {lat: 4.6998269, lng: -74.05737440000001}
             },{
                 'name': 'Mi Gran Parrilla Boyacense',
                 'facebook': 'https://www.facebook.com/parrillaboyacense/',
                 'address': 'Cl. 162 #22-68, Bogotá',
                 'cusine': 'Colombian',
-                'location': ""
+                'location': {lat: 4.743061700000001, lng: -74.0471475}
             },{
                 'name': 'Burger Music',
                 'facebook': 'https://www.facebook.com/BurgerMusic/',
                 'address': 'Cl. 116 #45 a 46, Bogotá',
                 'cusine': 'Fast Food',
+                'location': {lat: 4.6956824, lng: -74.0403925}
             },{
                 'name': 'Crepes & Waffles',
                 'facebook': 'https://es-la.facebook.com/CrepesyWafflesOficial/',
                 'address': 'Kr 19 #138-17, Bogotá',
                 'cusine': 'Colombian',
+                'location': {lat: 4.722226099999999, lng: -74.04655739999998}
             },{
                 'name': 'Pizzeria Da Quei Matti',
                 'facebook': 'https://www.facebook.com/daqueimatti/',
                 'address': 'Cra. 58 #128-45, Bogotá',
                 'cusine': 'Italian',
+                'location': {lat: 4.714338000000001, lng: -74.07017480000002}
             },{
                 'name': 'Il Mercatino',
                 'facebook': 'https://www.facebook.com/ilmercatinobog/',
                 'address': 'Cra. 10a #7050, Bogotá',
                 'cusine': 'Italian',
+                'location': {lat: 4.6549642, lng: -74.0591698}
             },{
                 'name': 'El Corral',
                 'facebook': 'https://es-la.facebook.com/hamburguesaselcorral',
                 'address': 'Cl. 138 #58-74, Bogotá',
                 'cusine': 'Fast Food',
+                'location': {lat: 4.7288122, lng: -74.06602750000002}
             },
         ];
         
-        var coordinates = [];
-        for(var j = 0; j<restaurants.length; j++){
-            console.dir(restaurants[j].address);
-            geocoder = new google.maps.Geocoder();
-            geocoder.geocode({'address': restaurants[j].address}, function(results, status){
-                if(status == google.maps.GeocoderStatus.OK) {
-//                    coordinates[j] = {
-//                       lat: results[0].geometry.location.lat(),
-//                       lng: results[0].geometry.location.lng()
-//                   };
-                    StoreLocation(results[0].geometry.location);
-                } else {
-                   window.alert("Geocoder error: "+ status);
-               }
-            });//geocoder
-            
-            function StoreLocation(result){
-                coordinates[j] = result.lat();
-            }
-        }; //end for
-                        console.log(coordinates);
-//        restaurants[j].location = location[j];
         localStorage.restaurants = JSON.stringify(restaurants);
     }
 };
@@ -87,4 +70,5 @@ var Restaurant = function(data){
     this.facebook = data.facebook;
     this.address = data.address;
     this.cusine = data.cusine;
+    this.location = data.location;
 }
--- js/viewmodel/viewmodel.js
+++ js/viewmodel/viewmodel.js
@@ -7,11 +7,9 @@ var viewModel = function(){
     self.foodTypes = ["All","Fast Food", "Italian", "Colombian", "Chinese", "Sushi"];
     self.selectedfoodType = ko.observable("Italian");
     
-    console.dir(self.selectedfoodType());
-    
     // populates restaurantsArray from restaurants
     restaurants.forEach(function(item){
-//        console.log(item);
+//        console.log(item.location);
         self.restaurantsArray.push(new Restaurant(item));
     });
     
@@ -21,40 +19,40 @@ var viewModel = function(){
     var myInfoWindow;
     // loop trough restaurants
     self.restaurantsArray().forEach(function(item){
-//        console.log(item);
+        console.log(item.location);
         myInfoWindow = new google.maps.InfoWindow();
                // create marker
-               var marker = new google.maps.Marker({
-                   map: map,
-                   position: item.location
-                   // position directly from object NO geocoding
-               });
+        var marker = new google.maps.Marker({
+            map: map,
+            position: item.location
+           // position directly from object NO geocoding
+        });
+
+       marker.addListener('click', function(){
+           populateInfoWindow(this, myInfoWindow);
+       });
                
-               marker.addListener('click', function(){
-                   populateInfoWindow(this, myInfoWindow);
+       function populateInfoWindow(marker, infowindow){
+           var title = infowindowTitle.replace("%data%",item.name);
+           var image = '<div id="pano"><div>';
+           var infoWindowContent = title + image;
+           if(infowindow.marker != marker){
+               infowindow.marker = marker;
+               infowindow.setContent(title + image);
+               infowindow.addListener('closeclick', function(){
+                  infowindow.marker = null; 
                });
-               
-               function populateInfoWindow(marker, infowindow){
-                   var title = infowindowTitle.replace("%data%",item.name);
-                   var image = '<div id="pano"><div>';
-                   var infoWindowContent = title + image;
-                   if(infowindow.marker != marker){
-                       infowindow.marker = marker;
-                       infowindow.setContent(title + image);
-                       infowindow.addListener('closeclick', function(){
-                          infowindow.marker = null; 
-                       });
-                       var sv = new google.maps.StreetViewService();
-                       var radius = 50;
-                       sv.getPanoramaByLocation(marker.position, radius, processSVData);;
-                       function processSVData(data,status){
-                           if(status == google.maps.StreetViewStatus.OK){
-                               var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"));
-                               panorama.setPosition(location);
-                           }
-                       }
-                       infowindow.open(map, marker);
+               var sv = new google.maps.StreetViewService();
+               var radius = 50;
+               sv.getPanoramaByLocation(marker.position, radius, processSVData);;
+               function processSVData(data,status){
+                   if(status == google.maps.StreetViewStatus.OK){
+                       var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"));
+                       panorama.setPosition(item.location);
                    }
-               };
+               }
+               infowindow.open(map, marker);
+           }
+       };
     });// end restaurant loop to add items to the map
 };
