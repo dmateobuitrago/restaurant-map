ef1eccd8b6e6998ae52d46bbae28c17d6f41ffb1
Mateo Buitrago <dmateobuitrago@gmail.com>
2016-10-16 11:51:53 -0500

    Leave just one forEach loop for the self.restauratsArray

--- js/viewmodel/viewmodel.js
+++ js/viewmodel/viewmodel.js
@@ -14,6 +14,8 @@ var viewModel = function(){
     self.selectedfoodType = ko.observable("All");
     
     self.filterRestaurants = ko.computed(function(){
+        var infowindowTitle = "<h3>%data%</h3>";
+        var myInfoWindow;
         self.restaurantsArray().forEach(function(item){
             //reset hidden items
             if(!item.visible()){
@@ -26,50 +28,42 @@ var viewModel = function(){
                 item.visible(false);
 //                console.log(self.selectedfoodType() + " != " + item.cusine());
             }
-            ////// end filter /////
-        });
-    });
-    
-    //infowindow content
-    var infowindowTitle = "<h3>%data%</h3>";
-    
-    var myInfoWindow;
-    // loop trough restaurants
-    self.restaurantsArray().forEach(function(item){
-//        console.log(item.location);
-        myInfoWindow = new google.maps.InfoWindow();
-               // create marker
-        var marker = new google.maps.Marker({
-            map: map,
-            position: item.location
-           // position directly from object NO geocoding
-        });
+            ////// end list filter /////
+        
+            myInfoWindow = new google.maps.InfoWindow();
+                   // create marker
+            var marker = new google.maps.Marker({
+                map: map,
+                position: item.location
+            });
 
-       marker.addListener('click', function(){
-           populateInfoWindow(this, myInfoWindow);
-       });
-               
-       function populateInfoWindow(marker, infowindow){
-           var title = infowindowTitle.replace("%data%",item.name());
-           var image = '<div id="pano"><div>';
-           var infoWindowContent = title + image;
-           if(infowindow.marker != marker){
-               infowindow.marker = marker;
-               infowindow.setContent(title + image);
-               infowindow.addListener('closeclick', function(){
-                  infowindow.marker = null; 
-               });
-               var sv = new google.maps.StreetViewService();
-               var radius = 50;
-               sv.getPanoramaByLocation(marker.position, radius, processSVData);;
-               function processSVData(data,status){
-                   if(status == google.maps.StreetViewStatus.OK){
-                       var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"));
-                       panorama.setPosition(item.location);
+            marker.addListener('click', function(){
+               populateInfoWindow(this, myInfoWindow);
+            });
+
+            function populateInfoWindow(marker, infowindow){
+               var title = infowindowTitle.replace("%data%",item.name());
+               var image = '<div id="pano"><div>';
+               var infoWindowContent = title + image;
+               if(infowindow.marker != marker){
+                   infowindow.marker = marker;
+                   infowindow.setContent(title + image);
+                   infowindow.addListener('closeclick', function(){
+                      infowindow.marker = null; 
+                   });
+                   var sv = new google.maps.StreetViewService();
+                   var radius = 50;
+                   sv.getPanoramaByLocation(marker.position, radius, processSVData);;
+                   function processSVData(data,status){
+                       if(status == google.maps.StreetViewStatus.OK){
+                           var panorama = new google.maps.StreetViewPanorama(document.getElementById("pano"));
+                           panorama.setPosition(item.location);
+                       }
                    }
+                   infowindow.open(map, marker);
                }
-               infowindow.open(map, marker);
-           }
-       };
-    });// end restaurant loop to add elements to the map
+            };
+        }); // end forEach loop restaurantsArray
+    }); // endfilterRestaurans ko.computed
+    
 };
